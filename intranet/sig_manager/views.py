d39165acb8804fa72e9cf87a36e0c4cb41466fde
Reed La Botz <reedlabotz@gmail.com>
2012-03-16 13:03:26 -0500

    work on sig form

--- intranet/sig_manager/views.py
+++ intranet/sig_manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.http import HttpResponse
 from intranet.sig_manager.models import SIG, SIGMember, Project
 
@@ -6,3 +6,10 @@ from intranet.sig_manager.models import SIG, SIGMember, Project
 def main(request):
   sigs = SIG.objects.all()
   return render_to_response('intranet/sig_manager/main.html',{"page":'sig','sigs':sigs})
+  
+def new(requset):
+  return render_to_response('intranet/sig_manager/form.html',{"page":'sig',"page_title":"Create new SIG"})
+  
+def edit(request,id):
+  return render_to_response('intranet/sig_manager/form.html',{"page":'sig',"page_title":"Edit SIG"})
+  
