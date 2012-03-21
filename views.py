e697e9cf61abea629ac95e08ad78e8cb3f426ea1
Cole Gleason <colewashere@gmail.com>
2012-03-21 01:58:24 -0500

    Added Github widget to front page.  It will need a lot of work to correctly format all events, but the basics are there

--- views.py
+++ views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+import urllib
+import simplejson
 
 def main(request):
-	return render_to_response('main.html')
+  # Build GitHub activity
+  feed = urllib.urlopen("https://api.github.com/orgs/acm-uiuc/events")
+  events = simplejson.loads(feed.read())[0:5]
+  return render_to_response('main.html', {'events':events})
 	
 def contact(request):
   return render_to_response('contact.html')
