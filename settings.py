812bfb8f349ea84d2da8ce615fdfdf5b781f59c2
root <root@ray.acm.uiuc.edu>
2012-04-11 10:02:51 -0500

    user in page

--- settings.py
+++ settings.py
@@ -202,6 +202,12 @@ AUTHENTICATION_BACKENDS = ('accounts.backends.ActiveDirectoryGroupMembershipSSLB
 
 CUSTOM_USER_MODEL = 'intranet.member_manager.models.Member'
 
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+"django.core.context_processors.debug",
+"django.core.context_processors.i18n",
+"django.core.context_processors.media",
+"django.contrib.messages.context_processors.messages")
+
 try:
   from local_settings import *
 except ImportError, exp:
