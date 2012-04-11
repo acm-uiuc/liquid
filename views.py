b6f9ecacdc4031e8b0c0bc107612756237a7a081
Reed La Botz <reedlabotz@gmail.com>
2012-04-11 01:35:12 -0500

    request context for main

--- views.py
+++ views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
 import urllib
 import simplejson
 
 def main(request):
-  return render_to_response('main.html',{})
+  return render_to_response('main.html',{},context_instance=RequestContext(request))
 	
 def contact(request):
   return render_to_response('contact.html',{"section":"contact"})
