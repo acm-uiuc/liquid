2f45afab4c871085eedf35aa4f40619dbd30fba9
Reed La Botz <reedlabotz@gmail.com>
2012-04-11 01:13:44 -0500

    use member directly

--- accounts/backends.py
+++ accounts/backends.py
@@ -24,22 +24,14 @@ class ActiveDirectoryGroupMembershipSSLBackend:
 
    def get_or_create_user(self, username, password):
       try:
-         user = self.user_class.objects.get(username=username)
-      except self.user_class.DoesNotExist:
+         user = Member.objects.get(username=username)
+      except Member.DoesNotExist:
          return None
 
       return user
 
    def get_user(self, user_id):
       try:
-         return self.user_class.objects.get(pk=user_id)
-      except self.user_class.DoesNotExist:
+         return Member.objects.get(pk=user_id)
+      except Member.DoesNotExist:
          return None
-
-   @property
-   def user_class(self):
-      if not hasattr(self, '_user_class'):
-         self._user_class = get_model(*settings.CUSTOM_USER_MODEL.split('.', 2))
-      if not self._user_class:
-         raise ImproperlyConfigured('Could not get custom user model')
-      return self._user_class
