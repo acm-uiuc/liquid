b0d39bc997eefd1761104d98ce56c1aeff1ed67d
Reed La Botz <reedlabotz@gmail.com>
2012-04-11 01:31:50 -0500

    remove middlewaer, use default

--- views.py
+++ views.py
@@ -8,9 +8,4 @@ def main(request):
 	
 def contact(request):
   return render_to_response('contact.html',{"section":"contact"})
-	
-def login(request):
-  user = None
-  if 'REMOTE_USER' in request.META:
-    user = request.META['REMOTE_USER']
-  return render_to_response('main.html',{'user':user})
+
