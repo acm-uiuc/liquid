ea050fdf00ee8f06efec3d93b8de95eda1bbbdb7
Reed La Botz <reedlabotz@gmail.com>
2012-04-17 17:44:35 -0500

    add pagination to events

--- liquid/cal/views.py
+++ liquid/cal/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from intranet.models import Event
 from django.template import RequestContext
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import datetime
 
 
 # Create your views here.
-def main(request):
-   futer = datetime.datetime.now() + datetime.timedelta(days=7)
-   events = Event.objects.filter(endtime__gte=datetime.datetime.now()).filter(starttime__lte=futer).order_by('starttime')
+def main(request,page=1):
+   events = Event.objects.filter(endtime__gte=datetime.datetime.now()).order_by('starttime')
+   paginator = Paginator(events, 10)
+   
+   try:
+      events = paginator.page(page)
+   except PageNotAnInteger:
+      # If page is not an integer, deliver first page.
+      events = paginator.page(1)
+   except EmptyPage:
+      # If page is out of range (e.g. 9999), deliver last page of results.
+      events = paginator.page(paginator.num_pages)
+
    return render_to_response('cal/main.html',{"section":"calendar","events":events},context_instance=RequestContext(request))
    
 def details(request,id):
