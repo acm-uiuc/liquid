84febc55917d36c6312814129fbb7559f5adedd3
Reed La Botz <reedlabotz@gmail.com>
2012-04-18 12:26:56 -0500

    fix calendar urls

--- liquid/cal/urls.py
+++ liquid/cal/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'cal.views.main'),
-    url(r'(?P<page>\d+)$', 'cal.views.main'),
-    url(r'details/(?P<id>\d+)$', 'cal.views.details',name='event_details'),
+    url(r'(?P<page>\d+)/$', 'cal.views.main'),
+    url(r'details/(?P<id>\d+)/$', 'cal.views.details',name='event_details'),
 )
 
 
