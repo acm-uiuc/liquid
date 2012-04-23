3c4b024d8075820b9011a68bafc0ad24589ee3f4
Reed La Botz <reedlabotz@gmail.com>
2012-04-23 13:23:21 -0500

    add chairs report

---
+++ liquid/scripts/chairs_report.py
@@ -0,0 +1,18 @@
+import sys,os
+sys.path.append(os.path.abspath('..'))
+
+from django.core.management import setup_environ 
+import settings 
+setup_environ(settings)
+
+
+from intranet.models import Group
+
+groups = Group.objects.all()
+
+for g in groups:
+   chairs = g.members.filter(groupmember__is_chair=True)
+   chair_netids = []
+   for c in chairs:
+      chair_netids.append(c.full_name_and_netid())
+   print "%s: %s\n" %(g.name,", ".join(chair_netids))
