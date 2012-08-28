8dbfe6b95219dc945d278f14548939c4d72d9125
Reed La Botz <reedlabotz@gmail.com>
2012-08-28 18:45:34 -0500

    Update text of vote email

--- liquid/scripts/send_vote.py
+++ liquid/scripts/send_vote.py
@@ -22,18 +22,32 @@ for m in members:
    v = Vote(user=m,key=key,poll=poll)
    v.save()
    email = """Dear %s,
-   
-   You are recieving this email because....
-   
-   To vote for this constitution please visit: http://acm.uiuc.edu/vote/%s/%s
-   
-   Thanks,
-   Reed La Botz
-   Secretary, ACM@UIUC
-   """ % (v.user.full_name(),v.user.username,v.key)
+
+We’ve been working on updating the ACM constitution to better reflect how we currently operate and to incorporate some new ideas. Under the guidelines of the current constitution, amendments have to be approved by our executive council and then by our membership. The proposed changes have been approved by exec, so now we’re sending it out to you so that you can take a look at it and vote on it. 
+
+Here’s a link to the new constitution https://www-s.acm.uiuc.edu/confluence/display/general/ACM+Constitution+-+Draft; there’s also a summary of the changes below. 
+
+Amendment highlights 
+Classifying members as undergraduate members, graduate members, faculty/staff members, active members and alumni members
+Updating duties of officers
+Not allowing officers to run for a second consecutive term in the same position 
+Making it possible for individuals to get funding for a project in addition to SIGs
+Defining what committees are and how they’re run
+Requiring 5 members to start a SIG
+Allowing for a designated proxy for SIG leadership at exec meetings 
+Defining the requirements of an active SIG and what happens when they’re no longer active 
+
+The vote is open for the next week, and can be accessed through this link: http://acm.uiuc.edu/vote/%s/%s. 
+
+If you have any questions, feel free to email top4@acm.uiuc.edu. 
+
+Thanks!
+
+Brianna Birman
+ACM@UIUC Chair""" % (v.user.full_name(),v.user.username,v.key)
    
    try:
-      send_mail('ACM Constitution', email, 'Reed La Botz<labotz1@illinois.edu>',[m.email], fail_silently=False)
+      send_mail('ACM Constitution', email, 'Brianna Birman<top4@acm.uiuc.edu>',[m.email], fail_silently=False)
       print "Email sent to %s" % m.full_name()
    except Exception as inst:
       print "Error sending email to %s" % m.full_name()
