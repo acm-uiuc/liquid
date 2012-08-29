355977d8859b8afb40d7751ad7b496eeca9d8c54
Reed La Botz <reedlabotz@gmail.com>
2012-08-29 16:52:40 -0500

    Let people return to the vote page.

--- liquid/vote/views.py
+++ liquid/vote/views.py
@@ -21,8 +21,5 @@ def vote(request,netid,key):
          return render_to_response('vote/thanks.html',{"name":vote.user.full_name(),"vote":vote_text },context_instance=RequestContext(request))
       except:
          pass
-   if not vote.vote == None:
-      #if they already voted
-      return render_to_response('vote/error.html',context_instance=RequestContext(request))
    return render_to_response('vote/vote.html',{"name":vote.user.full_name()},context_instance=RequestContext(request))
    
