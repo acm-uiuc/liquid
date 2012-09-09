2ed96fe6a924b2aa88e5ca07b1f0499736da3522
Reed La Botz <reedlabotz@gmail.com>
2012-09-09 11:07:07 -0500

    format events email nicer

--- liquid/scripts/events_email.py
+++ liquid/scripts/events_email.py
@@ -20,12 +20,17 @@ text = "Schedule for the week:\n"
 
 i = 1
 for e in events:
-   text += "%d. %s\n" %(i,e.name)
+   text += "%d. %s - %s\n" %(i,e.name,e.starttime.strftime('%m/%d/%y'))
    i += 1
 
 for e in events:
    text += "\n\n========================================================================\n\n"
-   text += "%s\n%s-%s\n%s\n\n%s" % (e.name,e.starttime.strftime('%b %d %Y %I:%M%p'),e.endtime.strftime('%b %d %Y %I:%M%p'),e.location,e.description)
+   time = e.starttime.strftime('%A, %b %d, %Y %I:%M%p')
+   if e.starttime.date() == e.endtime.date():
+      time += "-%s"%(e.endtime.strftime('%I:%M%p'))
+   else:
+      time += "-%s"%(e.endtime.strftime('%A, %b %d, %Y %I:%M%p'))
+   text += "%s\n%s\n%s\n\n%s" % (e.name,time,e.location,e.description)
 text += "\n\n========================================================================\n\n"
 
 print text
