a1c7d7bc8608601bf8f2aaf4e8ef6c0a2234aeec
Reed La Botz <reedlabotz@gmail.com>
2013-03-10 00:52:59 -0600

    fix 2013 url

--- liquid/conference/urls.py
+++ liquid/conference/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'conference.views.main'),
+    url(r'^2013/$', 'conference.views.main'),
     url(r'^subscribe$', 'conference.views.subscribe')
 )
 
