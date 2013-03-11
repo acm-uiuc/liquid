30733faff818799d2fff81ec6ab160183661d706
Cole Gleason <colewashere@gmail.com>
2013-03-11 16:50:04 -0500

    Override of ordering in lists in new group form

--- liquid/intranet/group_manager/forms.py
+++ liquid/intranet/group_manager/forms.py
@@ -1,11 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ModelChoiceField
 from django.forms.models import inlineformset_factory
 from intranet.models import Group, GroupMember
 from utils.widgets import SelectTimeWidget,BootstrapDateInput
+from utils.django_mailman.models import List
 
 GroupMemberFormSet = inlineformset_factory(Group, GroupMember,fields=('is_admin','is_chair','status'),can_delete=False,extra=0)
 
 class GroupForm(ModelForm):
+  mailing_list = ModelChoiceField(queryset=List.objects.order_by('name'))
   class Meta:
     model = Group
     exclude = ['members']
