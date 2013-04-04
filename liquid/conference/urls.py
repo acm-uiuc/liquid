2f67a8af0859ac1595a1982bff44530bae5145ae
Cole Gleason <colewashere@gmail.com>
2013-04-03 23:35:50 -0500

    Change manage.py crunchbang and correct some typos in resume

--- liquid/conference/urls.py
+++ liquid/conference/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'conference.views.main'),
     url(r'^2013/jobfair$', 'conference.views.jobfair'),
-    url(r'^2013/jobfair/invite$', 'conference.views.jobfair_invite'),
+    url(r'^jobfair$', 'conference.views.jobfair'),
     url(r'^2013/$', 'conference.views.main'),
     url(r'^subscribe/$', 'conference.views.subscribe')
 )
