e578121f9ccfeff575d4ced44ff6d50c0cdcf809
bjryan2 <bjryan2@illinois.edu>
2013-07-02 19:39:23 -0500

    fixed job sending pull -- added alert header and job count display

--- liquid/intranet/job_manager/urls.py
+++ liquid/intranet/job_manager/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'intranet.job_manager.views.main'),
-    url(r'^sendEmail', 'intranet.job_manager.views.send_email'),
+    url(r'^sendEmail', 'intranet.job_manager.views.send_job_email'),
 )
