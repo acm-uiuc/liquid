1d791f257f51b3f9830110a26b8042df8038d6f2
bjryan2 <bjryan2@illinois.edu>
2013-07-15 20:49:04 -0500

    added preview page for job email and test sender

--- liquid/intranet/job_manager/views.py
+++ liquid/intranet/job_manager/views.py
@@ -10,6 +10,7 @@ from utils.group_decorator import group_admin_required
 from utils.notifier_class import notif_class
 from django.core.mail import send_mail
 from scripts.send_job_email import send_email
+from scripts.send_job_email import view_email
 
 @group_admin_required(['Corporate'])
 def main(request):
@@ -21,10 +22,23 @@ def main(request):
    unsent_jobs = Job.objects.filter(sent__exact=False).filter(status__exact='approve')
    def_jobs = Job.objects.filter(sent__exact=False).filter(status__exact='defer')
    formset = JobFormSet(queryset=def_jobs)
-   return render_to_response('intranet/job_manager/main.html',{"section":"intranet","page":'jobs',"job_count":len(unsent_jobs), "def_job_count":len(def_jobs), "urgency": notif_class(unsent_jobs, 5), "jobs":formset},context_instance=RequestContext(request))
+   return render_to_response('intranet/job_manager/main.html',{"section":"intranet","page":"jobs","job_count":len(unsent_jobs), "def_job_count":len(def_jobs), "urgency": notif_class(unsent_jobs, 5), "jobs":formset},context_instance=RequestContext(request))
+
+@group_admin_required(['Top4'])
+def preview_email(request):
+    email = view_email()
+    return render_to_response('intranet/job_manager/email_preview.html',
+      {"section":"intranet", "page":"jobs", "email":email}, context_instance=RequestContext(request))
+
+@group_admin_required(['Top4'])
+def test_email(request):
+  addr = request.user.email
+  res = send_email(addr)
+  messages.add_message(request,messages.SUCCESS, res)
+  return HttpResponseRedirect('/intranet/jobs/previewEmail')
 
 @group_admin_required(['Top4'])
 def send_job_email(request):
-  send_email()
-  messages.add_message(request, messages.SUCCESS, 'Email Sent!')
+  res = send_email()
+  messages.add_message(request, messages.SUCCESS, res)
   return HttpResponseRedirect('/intranet/jobs')
--- liquid/scripts/send_job_email.py
+++ liquid/scripts/send_job_email.py
@@ -5,51 +5,60 @@ from django.core.management import setup_environ
 import settings
 setup_environ(settings)
 
-
 from intranet.models import Job
 from datetime import date
 from django.core.mail import send_mail
 from textwrap import wrap
 
+def gen_email(jobs):
 
-def send_email():
-   jobs = Job.objects.filter(sent__exact=False).filter(status__exact='approve')
+   today = date.today()
 
-   if jobs:
+   email = "ACM@UIUC Weekly Job Postings\r\nWeek of " + today.strftime("%m/%d/%y") + "\r\n\r\n"
+   email += "With postings from:\r\n"
 
-      today = date.today()
+   for j in jobs:
+      email += "* " + j.company + "\r\n"
 
-      email = "ACM@UIUC Weekly Job Postings\r\nWeek of " + today.strftime("%m/%d/%y") + "\r\n\r\n"
-      email += "With postings from:\r\n"
+   email += "\r\n========================================================================\r\n\r\n";
 
-      for j in jobs:
-         email += "* " + j.company + "\r\n"
-
+   for j in jobs:
+      email += "Title: " + j.job_title + "\r\n"
+      email += "Company: " + j.company + "\r\n";
+      email += "Contact: " + j.contact_name +  ' <' + j.contact_email + '> ' + j.contact_phone + "\r\n"
+      email += "Hiring for: " + j.types() + "\r\n"
+      description = j.description.split('\n')
+      description_out = ""
+      for d in description:
+         if d != "":
+            description_out += "\r\n".join(wrap(d,72)) + "\r\n"
+      email += "Description:\r\n" + description_out + "\r\n";
       email += "\r\n========================================================================\r\n\r\n";
 
-      for j in jobs:
-         email += "Title: " + j.job_title + "\r\n"
-         email += "Company: " + j.company + "\r\n";
-         email += "Contact: " + j.contact_name +  ' <' + j.contact_email + '> ' + j.contact_phone + "\r\n"
-         email += "Hiring for: " + j.types() + "\r\n"
-         description = j.description.split('\n')
-         description_out = ""
-         for d in description:
-            if d != "":
-               description_out += "\r\n".join(wrap(d,72)) + "\r\n"
-         email += "Description:\r\n" + description_out + "\r\n";
-         email += "\r\n========================================================================\r\n\r\n";
+   return email
 
-
+def send_email(addr="jobs-l@acm.uiuc.edu"):
+   jobs = Job.objects.filter(sent__exact=False).filter(status__exact='approve')
+   if jobs:
+      email = gen_email(jobs)
       try:
-         send_mail('ACM@UIUC Weekly Job Postings', email, 'ACM Corporate Committee <corporate@acm.uiuc.edu>',['jobs-l@acm.uiuc.edu'], fail_silently=False)
-         print "Email sent"
+         send_mail('ACM@UIUC Weekly Job Postings', email, 'ACM Corporate Committee <corporate@acm.uiuc.edu>',[addr], fail_silently=False)
+         return "Email sent to " + addr
          for j in jobs:
-            j.sent = True
-            j.save()
+             j.sent = True
+             j.save()
       except Exception as inst:
          print "Error sending email"
-         print inst
+         return "Error in sending email" + inst
 
    else:
-      print "No jobs to send"
+      return "No jobs to send"
+
+def view_email():
+   jobs = Job.objects.filter(sent__exact=False).filter(status__exact='approve')
+   if jobs:
+      email = gen_email(jobs)
+      return email
+   else:
+      return "No jobs to send"
+
