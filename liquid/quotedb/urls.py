fd1b3facbc18d8f7d37206ec4dfb210bd4d9935e
Ace Nassri <ace.nassri@gmail.com>
2013-09-01 02:50:12 -0500

    A few minor fixes

--- liquid/quotedb/urls.py
+++ liquid/quotedb/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'quotedb.views.main'),
-    url(r'^\?page=\d{0}+)$', 'quotedb.views.main'),
+    url(r'^\?page=\d{0,}$', 'quotedb.views.main'),
     url(r'^add/$', 'quotedb.views.add'),
 )
 
--- liquid/quotedb/views.py
+++ liquid/quotedb/views.py
@@ -9,6 +9,47 @@ from django.conf import settings
 # Create your views here.
 def main(request):
 
+   # Get commonly-computed values
+   quotes = getQuotes(request)
+   pages = getPages()
+
+   return render_to_response('quotedb/main.html',{"section":"quotedb","page":'main',"pages":pages,"quotes":quotes},context_instance=RequestContext(request))
+
+def add(request):
+
+   # Get commonly-computed values
+   quotes = getQuotes(request)
+   pages = getPages()
+
+   # Handle new quotes
+   if request.method == 'POST':
+
+      quoteForm = QuoteForm(request.POST)
+      quoteForm.save()
+
+      return render_to_response('quotedb/main.html',{"section":"quotedb","page":'main',"pages":pages,"quotes":quotes},context_instance=RequestContext(request))
+   else:
+      return render_to_response('quotedb/add.html',{"section":"quotedb","page":'add',"pages":pages,"form":QuoteForm,"quotes":quotes},context_instance=RequestContext(request))
+      
+# Helper function (gets a string list of quote page numbers)
+def getPages():
+   
+   # Misc. values used to set up quote pages
+   quotes_total = len(Quote.objects.all())
+   page_list = "1"
+   active_page = 1
+   
+   # Get string of quote page numbers
+   while (settings.QUOTES_PER_PAGE * active_page < quotes_total):
+   
+      page_list = page_list + str(active_page + 1)
+      active_page = active_page + 1
+      
+   return page_list
+   
+# Helper function #2 (gets a list of quotes given the request parameters)
+def getQuotes(request):
+
    # Misc. values used to set up quote pages
    quotes_total = len(Quote.objects.all())
    page_list = "1"
@@ -27,36 +68,4 @@ def main(request):
    quote_start_index = (page_number - 1) * settings.QUOTES_PER_PAGE
    quote_end_index = min(page_number * settings.QUOTES_PER_PAGE, quotes_total)
 
-   quotes = Quote.objects.all().order_by("created_at")[quote_start_index:quote_end_index]
-
-   return render_to_response('quotedb/main.html',{"section":"quotedb","page":'main',"pages":pages,"quotes":quotes},context_instance=RequestContext(request))
-
-def add(request):
-
-   pages = getPages()
-
-   # Handle new quotes
-   if request.method == 'POST':
-
-      quoteForm = QuoteForm(request.POST)
-      quoteForm.save()
-
-      return render_to_response('quotedb/main.html',{"section":"quotedb","page":'main',"pages":pages},context_instance=RequestContext(request))
-   else:
-      return render_to_response('quotedb/add.html',{"section":"quotedb","page":'add',"pages":pages,"form":QuoteForm},context_instance=RequestContext(request))
-      
-# Helper function (gets a string list of quote page numbers)
-def getPages():
-   
-   # Misc. values used to set up quote pages
-   quotes_total = len(Quote.objects.all())
-   page_list = "1"
-   active_page = 1
-   
-   # Get string of quote page numbers
-   while (settings.QUOTES_PER_PAGE * active_page < quotes_total):
-   
-      page_list = page_list + str(active_page + 1)
-      active_page = active_page + 1
-      
-   return page_list
+   return Quote.objects.all().order_by("-created_at")[quote_start_index:quote_end_index]
