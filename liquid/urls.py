78a335a134a487e4ef164dd9e27d4e5143639301
Ace Nassri <ace.nassri@gmail.com>
2014-08-28 22:23:04 -0500

    Case nit

--- liquid/urls.py
+++ liquid/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
     #url(r'^conference/', include('conference.urls')),
     url(r'^kiosk/$', 'kiosk.views.main'),
 
-    url(r'^vpnrequired/$', 'views.vpnrequired'),    
+    url(r'^vpnrequired/$', 'views.vpnRequired'),    
     url(r'^resume/$', redirect_to, {'url': '/corporate/resume/'}),
     url(r'^print/$', redirect_to, {'url': 
         'https://www-s.acm.uiuc.edu/confluence/display/admin/Printing'}),
