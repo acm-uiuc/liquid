3d245444c33dc70d33358508a0a4d8949f71751f
Cole Gleason <colewashere@gmail.com>
2015-01-21 11:45:04 -0600

    Revert "settings: use crowd for auth instead of ldap"
    This reverts commit e5e7c2d86767b041a22bc326c94efb0158f016d0. Users
    still getting 403.

--- liquid/settings.py
+++ liquid/settings.py
@@ -192,10 +192,25 @@ APPEND_SLASH = True
 
 SERVE_STATIC = True
 
-AUTHENTICATION_BACKENDS = (
-  'django.contrib.auth.backends.ModelBackend',
-  'crowd.backends.CrowdBackend',
-)
+
+## LDAP STUFF ## should be in local_settings for security
+
+#AD_DNS_NAME='your-ldap-server.com'
+# If using non-SSL use these
+#AD_LDAP_PORT=389
+#AD_LDAP_URL='ldap://%s:%s' % (AD_DNS_NAME,AD_LDAP_PORT)
+# If using SSL use these:
+#AD_LDAP_PORT=636
+#AD_LDAP_URL='ldaps://%s:%s' % (AD_DNS_NAME,AD_LDAP_PORT)
+#AD_SEARCH_DN='dc=mygroup,dc=net,dc=com'
+#AD_NT4_DOMAIN='YOURDOMAIN'
+#AD_SEARCH_FIELDS= ['mail','givenName','sn','sAMAccountName','memberOf']
+#AD_MEMBERSHIP_REQ=['Group_Required','Alternative_Group']
+#AD_CERT_FILE='/path/to/your/cert.txt'
+#AD_DEBUG=True
+#AD_DEBUG_FILE='/path/to/writable/log/file/ldap.debug'
+AUTHENTICATION_BACKENDS = ('utils.ldapauth.ActiveDirectoryGroupMembershipSSLBackend',
+                           'django.contrib.auth.backends.ModelBackend',)
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
@@ -228,6 +243,16 @@ LOGIN_REQUIRED_URLS = (
   (r'/corporate/resume/recruiter/(.*)$',False),
 )
 
+
+# ldap things
+AD_DNS_NAME= 'ad.uillinois.edu'
+AD_LDAP_PORT=389
+AD_LDAP_URL='ldap://%s:%s' % (AD_DNS_NAME,AD_LDAP_PORT)
+AD_BIND_DN='{0}@illinois.edu'
+AD_NT4_DOMAIN='AD.UILLINOIS.EDU'
+AD_SEARCH_FIELDS= ['givenName','sn','sAMAccountName']
+AD_DEBUG=True
+AD_DEBUG_FILE='/var/log/apache2/ldap.debug'
 
 CRON_IPS = ['172.22.32.110']
 CRON_PASSWORD = ""
