696b283bb688b5805c57f19a4177283ebfd29c7c
Ace Nassri <nassri2@illinois.edu>
2015-10-15 16:46:12 -0500

    Make logging more sensitive
    In-prod debugging for Caffeine

--- liquid/settings.py
+++ liquid/settings.py
@@ -171,14 +171,14 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'mail_admins': {
-            'level': 'ERROR',
+            'level': 'INFO',
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
-            'level': 'ERROR',
+            'level': 'INFO',
             'propagate': True,
         },
     }
